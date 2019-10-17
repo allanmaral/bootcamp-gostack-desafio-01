897183338ed55f7419bddf984ade18c438d2b757
Allan Amaral <allanmaral@hotmail.com>
2019-10-17 00:02:46 -0300

    Definied Project model

---
+++ src/model/Project.js
@@ -0,0 +1,42 @@
+class Project {
+  constructor() {
+    this.data = [];
+  }
+
+  create(id, title) {
+    this.data.push({
+      id,
+      title,
+      tasks: []
+    });
+  };
+
+  find(id) {
+    if (id) {
+      return this.data.filter(project => (project.id === id));
+    } else {
+      return this.data;
+    }
+  };
+
+  deleteAt(id) {
+    const index = this.data.findIndex(project => project.id === id);
+    if (index !== -1) {
+      this.data.splice(index, 1);
+    }
+  };
+
+  editAt(id, { title, tasks }) {
+    const index = this.data.findIndex(project => project.id === id);
+    if (index !== -1) {
+      if (title) {
+        this.data[index].title = title;
+      }
+      if (tasks) {
+        this.data[index].tasks.push(tasks);
+      }
+    }
+  };
+};
+
+module.exports = new Project();
